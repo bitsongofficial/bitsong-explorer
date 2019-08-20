fff96285fbf6e521636315bcde445785252bd465
Angelo <angelo@rcstart.it>
2019-08-20 03:23:01 +0200

    api testnet

--- assets/variables.js
+++ assets/variables.js
@@ -1,3 +1,3 @@
 export const BASE_TITLE = " — Wallet"
 export const PASSWORD_MIN_LENGTH = 8
-export const EXPLORER_API_URL = "http://localhost:8000"
+export const EXPLORER_API_URL = "https://api.testnet.bitsong.network"
