16ff19231b7457c4abefaae87d0825ae24958d06
Angelo <angelo@rcstart.it>
2019-08-20 03:29:39 +0200

    fix api url

--- assets/variables.js
+++ assets/variables.js
@@ -1,3 +1,3 @@
 export const BASE_TITLE = " — Wallet"
 export const PASSWORD_MIN_LENGTH = 8
-export const EXPLORER_API_URL = "https://api.testnet.bitsong.network"
+export const EXPLORER_API_URL = "https://api.testnet.explorebitsong.com"
