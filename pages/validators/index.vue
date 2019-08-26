b129b45bd3ba41a758731f0a8b3362df87c22f60
Angelo <angelo@rcstart.it>
2019-08-26 16:19:11 +0200

    page validators

--- pages/validators/index.vue
+++ pages/validators/index.vue
@@ -1,144 +1,194 @@
 <template>
   <v-container>
     <v-row no-gutters>
-      <v-col cols="12" xl="10" class="mx-auto mt-4">
-        <v-row no-gutters>
-          <v-col cols="6">
-            <h2 class="title">Validators</h2>
-          </v-col>
-          <v-col cols="6" class="text-right">
-            <v-btn
-              value="left"
-              :color="status === 'active' ? `primary` : ``"
-              @click="changeStatus('active')"
-            >Active</v-btn>
-            <v-btn
-              value="left"
-              :color="status === 'inactive' ? `primary` : ``"
-              @click="changeStatus('inactive')"
-            >Inactive</v-btn>
-          </v-col>
-        </v-row>
-
-        <v-data-table
-          :headers="tableHeaders"
-          :items="status === 'active' ? $store.getters[`validators/bonded`] : $store.getters[`validators/unbonded`]"
-          :items-per-page="100"
-          sort-by="delegator_shares"
-          sort-desc="true"
-          hide-default-footer
-          class="elevation-1 mt-4"
-        >
-          <template v-slot:body="{ items }">
-            <tr v-for="item in items" :key="item.name">
-              <td>
-                <v-avatar v-if="item.description.identity">
-                  <img :src="getProfilePic(item.description.identity)" alt="avatar" />
-                </v-avatar>
-                <nuxt-link
-                  class="pl-4 body-1"
-                  :to="`/validators/${item.operator_address}`"
-                >{{ item.description.moniker }}</nuxt-link>
-              </td>
-              <td>
-                <v-list-item-title class="body-2">{{ toPower(item.delegator_shares) | prettyRound }}</v-list-item-title>
-                <v-list-item-subtitle
-                  class="body-2"
-                  style="color:rgba(0,0,0,0.54)"
-                >{{ calculatePower(item.delegator_shares) }}%</v-list-item-subtitle>
-              </td>
-              <td>{{ percentage(item.commission.commission_rates.rate) }}%</td>
-            </tr>
-          </template>
-        </v-data-table>
+      <v-col cols="12" xl="8" class="mx-auto mt-4">
+        <v-card class="elevation-1">
+          <v-toolbar flat>
+            <v-toolbar-title>Validators</v-toolbar-title>
+            <div class="flex-grow-1"></div>
+            <v-menu bottom left transition="slide-y-transition">
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon>{{ sortDirection === 'asc' ? 'mdi-sort-ascending' : 'mdi-sort-descending' }}</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item @click="sortBy('voting_power')">
+                  <v-list-item-content>
+                    <v-list-item-title>Voting Power</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-icon>{{ sort === 'voting_power' ? (sortDirection === 'asc' ? 'mdi-sort-ascending' : 'mdi-sort-descending') : '' }}</v-icon>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-list-item @click="sortBy('moniker')">
+                  <v-list-item-content>
+                    <v-list-item-title>Moniker</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-icon>{{ this.sort === 'moniker' ? (sortDirection === 'asc' ? 'mdi-sort-ascending' : 'mdi-sort-descending') : '' }}</v-icon>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-list-item @click="sortBy('commission')">
+                  <v-list-item-content>
+                    <v-list-item-title>Commission</v-list-item-title>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-icon>{{ this.sort === 'commission' ? (sortDirection === 'asc' ? 'mdi-sort-ascending' : 'mdi-sort-descending') : '' }}</v-icon>
+                  </v-list-item-action>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12" xl="8" class="mx-auto mt-4">
+        <v-expansion-panels>
+          <v-expansion-panel v-for="(validator, i) in validators" :key="i" hide-actions>
+            <v-expansion-panel-header>
+              <v-row align="center" class="spacer" no-gutters>
+                <v-col cols="3" sm="3" md="1">
+                  <v-avatar size="36px" v-if="validator.details.description.profile_url">
+                    <img :src="validator.details.description.profile_url" />
+                  </v-avatar>
+                  <v-avatar size="36px" v-else v-html="avatar(validator.address)"></v-avatar>
+                </v-col>
+                <v-col sm="5" md="4">
+                  <div class="font-weight-medium">{{ validator.details.description.moniker }}</div>
+                  <div class="caption grey--text text--darken-1 hidden-sm-and-up mt-1">
+                    Voting Power {{ validator.voting_power | prettyRound }}
+                    &middot; Comm. {{ validator.details.commission.rate * 100 }}%
+                  </div>
+                </v-col>
+                <v-col class="grey--text text-truncate hidden-sm-and-down">
+                  Commission &mdash;
+                  <span
+                    class="grey--text text--darken-3"
+                  >{{ validator.details.commission.rate * 100 }}%</span>
+                </v-col>
+                <v-col
+                  class="grey--text text--darken-3 text-truncate mr-4 hidden-sm-and-down"
+                  align="right"
+                >
+                  <div>{{ validator.voting_power | prettyRound }}</div>
+                  <div class="caption">
+                    <span class="grey--text text--darken-1">Voting Power</span>
+                  </div>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <v-divider></v-divider>
+              <v-row no-gutter>
+                <v-col cols="12">
+                  <p
+                    class="mb-1"
+                    v-if="$vuetify.breakpoint.name === 'xs' || $vuetify.breakpoint.name === 'sm'"
+                  >{{ validator.details.operatorAddress | address }}</p>
+                  <p class="mb-1" v-else>{{ validator.details.operatorAddress }}</p>
+                  <div class="body-2 grey--text text--darken-2">Operator Address</div>
+                </v-col>
+                <v-col cols="12">
+                  <p
+                    class="mb-1"
+                    v-if="$vuetify.breakpoint.name === 'xs' || $vuetify.breakpoint.name === 'sm'"
+                  >{{ validator.address | address }}</p>
+                  <p class="mb-1" v-else>{{ validator.address }}</p>
+                  <div class="body-2 grey--text text--darken-2">Address</div>
+                </v-col>
+                <v-col cols="12" v-if="validator.details.description.website">
+                  <p class="mb-1">
+                    <a
+                      :href="validator.details.description.website"
+                    >{{ validator.details.description.website }}</a>
+                  </p>
+                  <div class="body-2 grey--text text--darken-2">Website</div>
+                </v-col>
+                <v-col cols="12">
+                  <p class="mb-1">{{ validator.details.commission.rate * 100 }}%</p>
+                  <div class="body-2 grey--text text--darken-2">Commission</div>
+                </v-col>
+                <v-col cols="12">
+                  <p class="mb-1">{{ validator.voting_power | prettyRound }} BTSG</p>
+                  <div class="body-2 grey--text text--darken-2">Voting Power</div>
+                </v-col>
+                <v-col cols="12" v-if="validator.details.description.details">
+                  <p class="mb-1">{{ validator.details.description.details }}</p>
+                  <div class="body-2 grey--text text--darken-2">Description</div>
+                </v-col>
+              </v-row>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-import config from "@/assets/config";
-import axios from "axios";
-import BigNumber from "bignumber.js";
-import { prettyRound } from "~/assets/utils";
+import { prettyRound, shortFilter } from "~/assets/utils";
+import jdenticon from "jdenticon";
+import gql from "graphql-tag";
 
 export default {
   filters: {
-    prettyRound
+    prettyRound,
+    address: value => shortFilter(value, 14)
+  },
+  apollo: {
+    validators: {
+      prefetch: true,
+      query: gql`
+        query Validators($sort: String, $sortDirection: String) {
+          validators(sort: $sort, sortDirection: $sortDirection) {
+            address
+            voting_power
+            proposer_priority
+            details {
+              operatorAddress
+              tokens
+              description {
+                moniker
+                identity
+                profile_url
+                website
+              }
+              commission {
+                rate
+              }
+            }
+          }
+        }
+      `,
+      variables() {
+        return {
+          sort: this.sort,
+          sortDirection: this.sortDirection
+        };
+      },
+      pollInterval: 5000
+    }
   },
   data() {
     return {
-      tableHeaders: [
-        {
-          text: `Moniker`,
-          align: `left`,
-          sortable: true,
-          value: `description.moniker`
-        },
-        {
-          text: `Voting Power`,
-          align: `left`,
-          sortable: true,
-          value: `delegator_shares`
-        },
-        {
-          text: `Commission %`,
-          align: `left`,
-          sortable: true,
-          value: `commission.commission_rates.rate`
-        }
-      ],
-      status: "active"
+      sort: "voting_power",
+      sortDirection: "desc"
     };
   },
-  created() {
-    this.$store.dispatch(`validators/getValidators`);
+  mounted() {
+    console.log();
   },
   methods: {
-    async getProfilePic(val) {
-      try {
-        const keybase = await axios
-          .get(
-            "https://keybase.io/_/api/1.0/user/lookup.json?fields=pictures&key_suffix=2474A8FCC4426BC5"
-          )
-          .then(res => res.data);
-        return keybase.them[0].pictures.primary.url;
-      } catch (error) {
-        console.error(error);
+    avatar(value) {
+      return jdenticon.toSvg(value, 36);
+    },
+    sortBy(value) {
+      if (value === this.sort) {
+        this.sortDirection = this.sortDirection === "asc" ? "desc" : "asc";
       }
-
-      //console.log(keybase.them[0].pictures.primary.url)
-
-      //return keybase.them[0].pictures.primary.url
-    },
-    calculatePower(share) {
-      const sharePower = new BigNumber(share);
-
-      return new BigNumber(sharePower)
-        .div(this.totalPower)
-        .multipliedBy(100)
-        .toFixed(2);
-    },
-    percentage(val) {
-      return new BigNumber(val).multipliedBy(100).toFixed(2);
-    },
-    changeStatus(val) {
-      this.status = val;
-    },
-    toPower(val) {
-      return new BigNumber(val).div(10 ** config.decimals).toFixed(0);
-    }
-  },
-  computed: {
-    totalPower() {
-      let validators = this.$store.getters[`validators/bonded`];
-      if (validators.length === 0) return new BigNumber(0).toFixed(0);
-
-      return validators.reduce((acc, val) => {
-        return new BigNumber(acc)
-          .plus(new BigNumber(val.delegator_shares))
-          .toFixed(0);
-      }, new BigNumber(0));
+      this.sort = value;
     }
   }
 };
