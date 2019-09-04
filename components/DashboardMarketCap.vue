4b003406ce25ee7718c1582636c1766ca0b9c1a8
Angelo <angelo@rcstart.it>
2019-09-05 01:44:10 +0200

    fix market cap

--- components/DashboardMarketCap.vue
+++ components/DashboardMarketCap.vue
@@ -9,7 +9,7 @@
         <v-list-item-subtitle>BTSG Price</v-list-item-subtitle>
       </v-list-item-content>
       <v-list-item-content>
-        <v-list-item-title>${{ totalCap | marketCap }}</v-list-item-title>
+        <v-list-item-title>-</v-list-item-title>
         <v-list-item-subtitle>Market Cap</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
@@ -34,39 +34,45 @@
 </template>
 
 <script>
-import BigNumber from "bignumber.js"
-import prettyNum, {PRECISION_SETTING} from 'pretty-num'
+import BigNumber from "bignumber.js";
+import prettyNum, { PRECISION_SETTING } from "pretty-num";
 import { pretty, prettyUsd, prettyRound, round } from "~/assets/utils";
 export default {
   filters: {
     prettyRound,
-    coinPrice: (value) => prettyNum(value, {precision: 4, precisionSetting: PRECISION_SETTING.FIXED}),
-    marketCap: (value) => {
-        const ROUND_POWER = 3;
-        value = value.toString()
-        if (value === "NaN") value = 0
-        if (value > Math.pow(10, 9)) {
-            return round(value / Math.pow(10, 9), ROUND_POWER) + ' billion';
-        }
-        if (value > Math.pow(10, 6)) {
-            return round(value / Math.pow(10, 6), ROUND_POWER) + ' million';
-        }
-        return prettyUsd(value);
-    },
+    coinPrice: value =>
+      prettyNum(value, {
+        precision: 4,
+        precisionSetting: PRECISION_SETTING.FIXED
+      }),
+    marketCap: value => {
+      const ROUND_POWER = 3;
+      value = value.toString();
+      if (value === "NaN") value = 0;
+      if (value > Math.pow(10, 9)) {
+        return round(value / Math.pow(10, 9), ROUND_POWER) + " billion";
+      }
+      if (value > Math.pow(10, 6)) {
+        return round(value / Math.pow(10, 6), ROUND_POWER) + " million";
+      }
+      return prettyUsd(value);
+    }
   },
   computed: {
-    totalTxs () {
-      return this.$store.getters['connection/lastHeader'].total_txs
+    totalTxs() {
+      return this.$store.getters["connection/lastHeader"].total_txs;
     },
-    lastBlock () {
-      return this.$store.getters['connection/lastHeader'].height
+    lastBlock() {
+      return this.$store.getters["connection/lastHeader"].height;
     },
-    totalCap () {
-      return new BigNumber(this.$store.getters['pool/total_supply']).multipliedBy(this.price)
+    totalCap() {
+      return new BigNumber(
+        this.$store.getters["pool/total_supply"]
+      ).multipliedBy(this.price);
     },
-    price () {
-      return this.$store.getters['market/price']
+    price() {
+      return this.$store.getters["market/price"];
     }
   }
-}
+};
 </script>
