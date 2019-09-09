af9eef2aed1114400dbbef7dbfd07282fbe001f5
Angelo <angelo@rcstart.it>
2019-09-09 17:22:11 +0200

    fix logo

--- assets/variables.js
+++ assets/variables.js
@@ -1,11 +1,11 @@
 const BASE_SHORT_TITLE = process.env.BASE_SHORT_TITLE
   ? process.env.BASE_SHORT_TITLE
-  : "BitSong";
+  : "Tichex";
 
 export const MAINNET = "mainnet";
 export const TESTNET = "testnet";
 export const NETWORK = process.env.TESTNET ? TESTNET : MAINNET;
-export const BASE_TITLE_NETWORK = NETWORK === MAINNET ? "Mainnet " : "Testnet ";
+export const BASE_TITLE_NETWORK = NETWORK === MAINNET ? "Testnet " : "Testnet ";
 export const BASE_TITLE_START =
   " — " + BASE_SHORT_TITLE + " " + BASE_TITLE_NETWORK;
 export const BASE_TITLE = BASE_TITLE_START + "Explorer";
