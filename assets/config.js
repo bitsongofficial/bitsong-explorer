9bc2ea011ac998bd7c038cc01ee22099ddacec6e
Angelo <angelo@rcstart.it>
2019-09-27 18:59:49 +0200

    Fix stake token supply

--- assets/config.js
+++ assets/config.js
@@ -1,32 +1,32 @@
 const dev = process.env.NODE_ENV === `development`;
 const stargate =
   process.env.STARGATE ||
-  (dev ?
-    `https://lcd.testnet-2.bitsong.network` :
-    `https://lcd.testnet-2.bitsong.network`);
+  (dev
+    ? `https://lcd.tichex-devnet-1.bas.network`
+    : `https://lcd.tichex-devnet-1.bas.network`);
 
 const rpc =
   process.env.RPC ||
-  (dev ?
-    `https://rpc.testnet-2.bitsong.network` :
-    `https://rpc.testnet-2.bitsong.network`);
+  (dev
+    ? `https://rpc.tichex-devnet-1.bas.network`
+    : `https://rpc.tichex-devnet-1.bas.network`);
 
 const gql =
   process.env.GQL ||
-  (dev ?
-    `http://localhost:8081/gql/graphql` :
-    `https://testnet.explorebitsong.com/gql/graphql`);
+  (dev
+    ? `https://tichex-devnet-1.bas.network/gql/graphql`
+    : `https://tichex-devnet-1.bas.network/gql/graphql`);
 const gql_ws =
   process.env.GQL_WS ||
-  (dev ?
-    `ws://localhost:8081/gql/subscriptions` :
-    `wss://testnet.explorebitsong.com/gql/subscriptions`);
+  (dev
+    ? `wss://tichex-devnet-1.bas.network/gql/subscriptions`
+    : `wss://tichex-devnet-1.bas.network/gql/subscriptions`);
 
-const stakeDenom = process.env.STAKE_DENOM ? process.env.STAKE_DENOM : "ubtsg";
+const stakeDenom = process.env.STAKE_DENOM ? process.env.STAKE_DENOM : "utcx";
 
 export default {
   development: dev,
-  network: process.env.NETWORK || `bitsong-testnet-2`,
+  network: process.env.NETWORK || `tichex-devnet-1`,
   stargate,
   rpc,
   gql,
